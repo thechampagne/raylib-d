f95857cd8d880f109cdd7cd9c201a7f2d826a112
XXIV <13811862+thechampagne@users.noreply.github.com>
2022-08-02 01:39:00 +0300

    Add core_basic_window example

---
+++ 01_core_basic_window/source/app.d
@@ -0,0 +1,44 @@
+import raylib;
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main()
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+            ClearBackground(Colors.RAYWHITE);
+
+            DrawText("Congrats! You created your first window!", 190, 200, 20, Colors.LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 
 Examples using raylib core platform functionality like window creation, inputs, drawing modes and system functionality.
 
+| ## | example  | image  |
+|----|----------|--------|
+| 01 | [core_basic_window](01_core_basic_window/source/app.d) | <img src="https://raw.githubusercontent.com/raysan5/raylib/master/examples/core/core_basic_window.png" alt="core_basic_window" width="80"> |
+
 ### License
 
 This repo is released under the [MIT](https://github.com/thechampagne/raylib-d/blob/main/LICENSE).
